3f7f894a1ab424ec6edb8215e19d08892282f4c8
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-01-29 22:55:25 +0000

    Decouple frontend and backend functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import backend
 
 def get_pll(data):
 	input_pll = input("What pll would you like to see the result of?\n")
@@ -6,28 +7,6 @@ def get_pll(data):
 	for i in data: # Loop through all the pll's
 		if i["Name"] == input_pll: # If the name of the current item maches the user input, return this item
 			return i
-
-def run_pll(array_input, transform):
-	out = [0]*9
-	for (index, i) in enumerate(transform):
-		out[index] = array_input[i]
-	
-	return out
-
-def detect_pll(pll, data):
-	for i in data:
-		res = run_pll(pll, i["Transform"])
-		for j in range(4):
-			if res == [0, 1, 2, 3, 4, 5, 6, 7, 8]:
-				return i
-			res = run_pll(res, [6, 3, 0, 7, 4, 1, 8, 5, 2]) # U move
-
-def get_pll_case(input_pll, data): 
-	for i in range(4): # Loop 4 times with a U move in between to account for all orientations
-		resultant_pll = detect_pll(input_pll, data) #Get pll for this case
-		if resultant_pll != None: # If there is a pll for this case, break out the function
-			return resultant_pll
-		input_pll = run_pll(input_pll, [6, 3, 0, 7, 4, 1, 8, 5, 2]) # U move
 	
 
 def main():
@@ -40,10 +19,10 @@ def main():
 	first_pll = get_pll(data)
 	second_pll = get_pll(data)
 
-	result1 = run_pll([0, 1, 2, 3, 4, 5, 6, 7, 8], first_pll["Transform"])
-	result2 = run_pll(result1, second_pll["Transform"])
+	result1 = backend.run_pll([0, 1, 2, 3, 4, 5, 6, 7, 8], first_pll["Transform"])
+	result2 = backend.run_pll(result1, second_pll["Transform"])
 
-	resultant_pll = get_pll_case(result2, data)
+	resultant_pll = backend.get_pll_case(result2, data)
 	print("These PLL's would result in a", resultant_pll["Name"], "perm")
 
 main()
