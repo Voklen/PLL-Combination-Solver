7f2fee305483c365b0d8e81185bd0d13960c3303
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-01-30 11:57:15 +0000

    Simplify main() and move more code to backend

--- backend.py
+++ backend.py
@@ -19,3 +19,10 @@ def get_pll_case(input_pll, data):
 		if resultant_pll != None: # If there is a pll for this case, break out the function
 			return resultant_pll
 		input_pll = run_pll(input_pll, [6, 3, 0, 7, 4, 1, 8, 5, 2]) # U move
+
+def get_resultant_pll(first_pll, second_pll, data):
+	result1 = run_pll([0, 1, 2, 3, 4, 5, 6, 7, 8], first_pll["Transform"])
+	result2 = run_pll(result1, second_pll["Transform"])
+
+	resultant_pll = get_pll_case(result2, data)
+	return resultant_pll
--- main.py
+++ main.py
@@ -7,9 +7,8 @@ def get_pll(data):
 	for i in data: # Loop through all the pll's
 		if i["Name"] == input_pll: # If the name of the current item maches the user input, return this item
 			return i
-	
 
-def main():
+def get_input():
 	print("Welcome to my pll result program")
 	print("Made by Alex Gorichev")
 	
@@ -18,11 +17,11 @@ def main():
 	
 	first_pll = get_pll(data)
 	second_pll = get_pll(data)
+	return data, first_pll, second_pll
 
-	result1 = backend.run_pll([0, 1, 2, 3, 4, 5, 6, 7, 8], first_pll["Transform"])
-	result2 = backend.run_pll(result1, second_pll["Transform"])
-
-	resultant_pll = backend.get_pll_case(result2, data)
+def main():
+	data, first_pll, second_pll = get_input()
+	resultant_pll = backend.get_resultant_pll(first_pll, second_pll, data)
 	print("These PLL's would result in a", resultant_pll["Name"], "perm")
 
 main()
